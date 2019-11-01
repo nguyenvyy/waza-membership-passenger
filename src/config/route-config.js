2109ee35f23750f6d6c948aade3106569baf9ff2
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-01 09:46:39 +0700

    add Login page

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ import { Redirect } from 'react-router-dom'
 import { lazy } from 'react'
 
 import { PrivateRoute } from '../routes/PrivateRoute'
+
 const Passenger = lazy(() => import('../components/Passenger/Passenger'))
+const Login = lazy(() => import('../components/Login/Login'))
 
 export const routes = [
     {
@@ -15,5 +17,9 @@ export const routes = [
         path: '/p',
         component: Passenger,
         customRoute: PrivateRoute,
+    },
+    {
+        path: '/login',
+        component: Login
     }
 ]
