8e565055d1158486dc9b98b2cbd042fcffb2d1c5
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-14 00:20:56 +0700

    add group combo by policy selector

---
+++ src/redux/selector/combo.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect"
+export const findPolicyById = (policies, id) => policies.find(policy => policy._id === id) 
+export const getCombo = activeCombo => activeCombo.items
+export const getPolicies = activeCombo => activeCombo.policies
+export const groupCombosByPolicy = createSelector(
+    [getCombo, getPolicies],
+    (combos, policies) => {
+        if(combos.length > 0 && policies.length > 0) {
+            let result = combos.reduce((acc, curr) => {
+                const policy = findPolicyById(policies, curr.policy_id)
+                if(acc[curr.policy_id] === undefined) {
+                    acc[curr.policy_id] = {
+                        combos: [curr],
+                        policy
+                    }
+                } else {
+                    acc[curr.policy_id].combos.push(curr)
+                }
+                
+                return acc
+            }, {})
+            return result
+        } else {
+            return {}
+        }
+    }
+)
