6307c03b393c703c6df788e1ab114162e879ebec
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-14 22:39:09 +0700

    update data not found

--- src/components/common/Empty/Empty.js
+++ src/components/common/Empty/Empty.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export const NotData = () => (<div>data is empty..</div>)
 
-export const Empty = ({ isEmpty = false, alternative = <NotData />, children }) => (
+export const Empty = ({ isEmpty = false, alternative = <div>not found....</div>, children }) => (
     <>
         {isEmpty ? alternative : children}
     </>
