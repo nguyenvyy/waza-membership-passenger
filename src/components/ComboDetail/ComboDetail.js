53d58b0b85714c3ccff4795b35aca52ab40a3dce
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-20 17:59:39 +0700

    add route for combo detail

---
+++ src/components/ComboDetail/ComboDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ComboDetail.scss'
+const ComboDetail = ({combo}) => {
+
+    return (
+        <div>
+            'this is my ComboDetail'
+        </div>
+    )
+}
+
+export default ComboDetail
